6306eb23bb4594b0bb44b4443decbd0d534e00dd
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-04-19 19:54:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+class Nod{
+public:
+    int key;
+    int prio;
+    Nod* left;
+    Nod* right;
+
+    Nod(){
+    this->key = 0 ;
+    this-> prio = 0;
+    this->left = NULL;
+    this->right = NULL;
+    }
+
+    Nod(int key,int prio, Nod* left,Nod* right){
+        this->key = key;
+        this->prio = prio;
+        this->left = left;
+        this->right = right;
+    }
+    bool operator ==(const Nod& obj){
+    if(this->key = key && this->prio == prio && this->left == left && this->right == right) return 1;
+    return 0;
+    }
+    friend ostream& operator<<(ostream& out,const Nod &obj){
+        Nod gol;
+        if(gol == obj){out<<NULL; return out;}
+        out<<endl<<'('<<obj.key<<','<<obj.prio<<')'<<endl;
+        out<<"CopSt:"<<obj.left<<endl;
+        out<<"CopDr:"<<obj.right<<endl;
+        out<<"Adresa:"<<&obj<<endl;
+        return out;
+    }
+};
+
+Nod* nodNou(int key){
+    Nod* nou = new Nod(key,rand()%100,NULL,NULL);
+    return nou;
+}
+Nod* nodNou(int key,int prio){
+    Nod* nou = new Nod(key,prio,NULL,NULL);
+    return nou;
+}
+
+
+Nod* cautare(Nod* &rad , int key){
+    if(rad == NULL) return NULL;
+    if(rad->key == key) return rad;
+    if(rad->key < key) cautare(rad->right ,key);
+    else cautare(rad->left ,key);
+}
+
+Nod* rotDr(Nod* &nod){
+    Nod* aux = nod->left;
+    Nod* aux2 = aux->right;
+    aux->right =  nod;
+    nod->left = aux2;
+    return aux;
+}
+Nod* rotSt(Nod* &nod){
+    Nod* aux = nod->right;
+    Nod* aux2 = aux->left;
+    aux->left = nod;
+    nod->right = aux2;
+    return aux;
+}
+
+void afisare(Nod* rad){
+    if(rad != NULL){
+        cout<<*rad<<endl;
+        afisare(rad->left);
+        afisare(rad->right);
+    }
+}
+
+void inorder(Nod* root)
+{
+    if (!root)
+        return;
+
+    inorder(root->left);
+    cout <<'('<< root->key<<','<<root->prio<<')' << " ";
+    inorder(root->right);
+}
+Nod* inserare(Nod* rad,int key){
+    if(rad == NULL){
+        rad = nodNou(key);
+        return rad;
+    }
+    if(key <= rad->key){
+        rad->left = inserare(rad->left,key);
+        if(rad->left->prio > rad->prio)
+            rad = rotDr(rad);
+    }
+    else{
+        rad->right = inserare(rad->right,key);
+        if(rad->right->prio > rad->prio)
+            rad = rotSt(rad);
+    }
+    return rad;
+}
+Nod* inserare(Nod* rad,int key,int prio){
+    if(rad == NULL){
+        rad = nodNou(key,prio);
+        return rad;
+    }
+    if(key <= rad->key){
+        rad->left = inserare(rad->left,key,prio);
+        if(rad->left->prio > rad->prio)
+            rad = rotDr(rad);
+    }
+    else{
+        rad->right = inserare(rad->right,key,prio);
+        if(rad->right->prio > rad->prio)
+            rad = rotSt(rad);
+    }
+    return rad;
+}
+Nod* build(int v[], int n){
+    if (n == 0)
+        return NULL;
+    Nod* rad = NULL;
+
+    for (int i = 0; i < n; i++)
+        rad = inserare(rad, v[i],rand()%100);
+
+    return rad;
+}
+Nod* minim(Nod* &rad){
+    if(rad->left != NULL)
+        return minim(rad->left);
+    else
+        return rad;
+}
+Nod* maxim(Nod* &rad){
+    if(rad->right != NULL)
+        return maxim(rad->right);
+    else
+        return rad;
+}
+/*
+Nod* stergere(Nod* nod,int key){
+    if(nod == NULL) return NULL;
+    if(nod->left == NULL && nod->right == NULL){
+        delete nod;
+        nod = NULL;
+        return nod;
+    }
+
+    if(key < nod->key)
+        nod->left = stergere(nod->left , key);
+
+    else if(key > nod->key)
+        nod->right = stergere(nod->right , key);
+
+}
+*/
+int main()
+{srand(time(nullptr));
+    int vecPerechi[] = { 10,40,20,50,30,60};
+    Nod* rad = build(vecPerechi,sizeof(vecPerechi)/sizeof(int));
+    rad = inserare(rad,15);
+    rad = inserare(rad,25,69);
+    //cout<<cautare(rad,11);
+    //inorder(rad);
+    //cout<<*rotDr(rad->right);
+    //afisare(rad);
+    //cout<<*minim(rad);
+    //cout<<*maxim(rad);
+    //cout<<stergere(rad,60);
+}
