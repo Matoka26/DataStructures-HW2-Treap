ed761ebe2505019a481d613f3240fec1eaee4509
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-04-21 00:10:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,24 +8,27 @@ using namespace std;
 
 int main()
 {srand(time(nullptr));
-    int vecPerechi[] = { 10,40,20,50,30,60};
-    Nod *a,*b;
-    Nod* rad = build(vecPerechi,sizeof(vecPerechi)/sizeof(int));
+    int vecPerechi[] = {10,40,20,50,30,60};
+    Treap *a,*b;
+    Treap* rad = build(vecPerechi,sizeof(vecPerechi)/sizeof(int));
     rad = inserare(rad,15);
     rad = inserare(rad,25,69);
-    cout<<cautare(rad,11);
+    ///cout<<cautare(rad,10);
     ///inorder(rad);
-    ///afisare(rotDr(rad->right));      ///rotatiile s ar putea sa dea segfault
-    ///afisare(rotSt(rad->left));       ///sunt implementate auxiliar
-    ///afisare(rad);                    ///nu sa le folosesti asa oricand
-    ///rad = stergere(rad,40);
+    ///afisare(rotDr(rad->right));      rotatiile s ar putea sa dea segfault
+    ///afisare(rotSt(rad->left));       sunt implementate auxiliar
+    ///afisare(rad);                    nu sa le folosesti asa oricand
+    ///rad = stergere(rad,30);
     ///afisareSplit(rad,30);
     ///split(rad,a,b,30);
     ///afisare(join(a,b));
+    ///inorder(join(a,b));
+    ///inorder(a);
     ///rad = reuniune(a,b);
-    ///dezalocare(rad);
     ///cout<<*minim(rad);
     ///cout<<*maxim(rad);
+    ///interval(rad,10,50);
+///dezalocare(rad);
 //dezalocare(a);
 //dezalocare(b);
 return 0;
