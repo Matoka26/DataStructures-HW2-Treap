110a093735afc44aec45f59b855d9c7939c6310d
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-04-20 21:49:26 +0300

    Update treap.h

--- treap.h
+++ treap.h
@@ -1,31 +1,36 @@
 #include<iostream>
-using namespace std;
-class Nod{
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+using std::cout;
+using std::endl;
+using std::ostream;
+class Treap{
 public:
     int key;
     int prio;
-    Nod* left;
-    Nod* right;
+    Treap* left;
+    Treap* right;
 
-    Nod(){
+    Treap(){
     this->key = 0 ;
     this-> prio = 0;
     this->left = NULL;
     this->right = NULL;
     }
 
-    Nod(int key,int prio, Nod* left,Nod* right){
+    Treap(int key,int prio, Treap* left,Treap* right){
         this->key = key;
         this->prio = prio;
         this->left = left;
         this->right = right;
     }
-    bool operator ==(const Nod& obj){
+    bool operator ==(const Treap& obj){
     if(this->key = key && this->prio == prio && this->left == left && this->right == right) return 1;
     return 0;
     }
-    friend ostream& operator<<(ostream& out,const Nod &obj){
-        Nod gol;
+    friend ostream& operator<<(ostream& out,const Treap &obj){
+        Treap gol;
         if(gol == obj){out<<NULL; return out;}
         out<<endl<<'('<<obj.key<<','<<obj.prio<<')'<<endl;
         out<<"CopSt:"<<obj.left<<endl;
@@ -35,39 +40,35 @@ public:
     }
 };
 
-Nod* nodNou(int key){
-    Nod* nou = new Nod(key,rand()%100,NULL,NULL);
-    return nou;
-}
-Nod* nodNou(int key,int prio){
-    Nod* nou = new Nod(key,prio,NULL,NULL);
+Treap* TreapNou(int key,int prio){
+    Treap* nou = new Treap(key,prio,NULL,NULL);
     return nou;
 }
 
 
-Nod* cautare(Nod* &rad , int key){
+Treap* cautare(Treap* &rad , int key){
     if(rad == NULL) return NULL;
     if(rad->key == key) return rad;
     if(rad->key < key) cautare(rad->right ,key);
     else cautare(rad->left ,key);
 }
 
-Nod* rotDr(Nod* &nod){
-    Nod* aux = nod->left;
-    Nod* aux2 = aux->right;
-    aux->right =  nod;
-    nod->left = aux2;
+Treap* rotDr(Treap* &rad){
+    Treap* aux = rad->left;
+    Treap* aux2 = aux->right;
+    aux->right =  rad;
+    rad->left = aux2;
     return aux;
 }
-Nod* rotSt(Nod* &nod){
-    Nod* aux = nod->right;
-    Nod* aux2 = aux->left;
-    aux->left = nod;
-    nod->right = aux2;
+Treap* rotSt(Treap* &rad){
+    Treap* aux = rad->right;
+    Treap* aux2 = aux->left;
+    aux->left = rad;
+    rad->right = aux2;
     return aux;
 }
 
-void afisare(Nod* rad){
+void afisare(Treap* rad){
     if(rad != NULL){
         cout<<*rad<<endl;
         afisare(rad->left);
@@ -75,7 +76,7 @@ void afisare(Nod* rad){
     }
 }
 
-void inorder(Nod* rad)
+void inorder(Treap* rad)
 {
     if (rad == NULL)
         return;
@@ -84,9 +85,9 @@ void inorder(Nod* rad)
     cout <<'('<< rad->key<<','<<rad->prio<<')' << " ";
     inorder(rad->right);
 }
-Nod* inserare(Nod* rad,int key){
+Treap* inserare(Treap* rad,int key){
     if(rad == NULL){
-        rad = nodNou(key);
+        rad = TreapNou(key,rand()%100000000);
         return rad;
     }
     if(key <= rad->key){
@@ -101,9 +102,9 @@ Nod* inserare(Nod* rad,int key){
     }
     return rad;
 }
-Nod* inserare(Nod* rad,int key,int prio){
+Treap* inserare(Treap* rad,int key,int prio){
     if(rad == NULL){
-        rad = nodNou(key,prio);
+        rad = TreapNou(key,prio);
         return rad;
     }
     if(key <= rad->key){
@@ -118,71 +119,71 @@ Nod* inserare(Nod* rad,int key,int prio){
     }
     return rad;
 }
-Nod* build(int v[], int n){
+Treap* build(int v[], int n){
     if (n == 0)
         return NULL;
-    Nod* rad = NULL;
+    Treap* rad = NULL;
 
     for (int i = 0; i < n; i++)
-        rad = inserare(rad, v[i],rand()%100);
+        rad = inserare(rad, v[i],rand()%100000000);
 
     return rad;
 }
-Nod* minim(Nod* &rad){
+Treap* minim(Treap* &rad){
     if(rad->left != NULL)
         return minim(rad->left);
     else
         return rad;
 }
-Nod* maxim(Nod* &rad){
+Treap* maxim(Treap* &rad){
     if(rad->right != NULL)
         return maxim(rad->right);
     else
         return rad;
 }
 
-Nod* stergere(Nod* &nod,int key){
-    if(nod == NULL) return NULL;    //arborele e gol
+Treap* stergere(Treap* &rad,int key){
+    if(rad == NULL) return NULL;    //arborele e gol
 
-    if(key < nod->key)                 //caut keya
-        nod->left = stergere(nod->left , key);
-    else if(key > nod->key)
-        nod->right = stergere(nod->right , key);
+    if(key < rad->key)                 //caut keya
+        rad->left = stergere(rad->left , key);
+    else if(key > rad->key)
+        rad->right = stergere(rad->right , key);
 
-    else if(key == nod->key){            //am gasit keya
-        if(nod->left ==NULL){           //n are copil in stanga,il ridic pe ala din dreapta
-            Nod* aux = nod->right;
-            delete nod;
-            nod = aux;
+    else if(key == rad->key){            //am gasit keya
+        if(rad->left ==NULL){           //n are copil in stanga,il ridic pe ala din dreapta
+            Treap* aux = rad->right;
+            delete rad;
+            rad = aux;
         }
-        else if(nod->right == NULL){    //n are copil in dreapta,il ridic pe ala din stanga
-            Nod* aux = nod->left;
-            delete nod;
-            nod = aux;
+        else if(rad->right == NULL){    //n are copil in dreapta,il ridic pe ala din stanga
+            Treap* aux = rad->left;
+            delete rad;
+            rad = aux;
         }
-    else if(nod->left->prio < nod->right->prio){
-        nod = rotSt(nod);
-        nod->left = stergere(nod->left,key);
+    else if(rad->left->prio < rad->right->prio){
+        rad = rotSt(rad);
+        rad->left = stergere(rad->left,key);
         }
-    else if(nod->right->prio <= nod->left->prio){
-        nod = rotDr(nod);
-        nod->right = stergere(nod->right , key);
+    else if(rad->right->prio <= rad->left->prio){
+        rad = rotDr(rad);
+        rad->right = stergere(rad->right , key);
         }
     }
-    return nod;
+    return rad;
 }
-void dezalocare(Nod* &nod){
-    if(nod != NULL){
-        if(nod->left != NULL)
-            dezalocare(nod->left);
-        if(nod->right != NULL)
-            dezalocare(nod->right);
-    delete nod;
-    nod = NULL;
+void dezalocare(Treap* &rad){
+    if(rad != NULL){
+        if(rad->left != NULL)
+            dezalocare(rad->left);
+        if(rad->right != NULL)
+            dezalocare(rad->right);
+    delete rad;
+    rad = NULL;
     }
 }
 
-void afisareSplit(Nod* &rad,int key){
+void afisareSplit(Treap* &rad,int key){
     rad = inserare(rad,key,INFINITY);
     cout<<"LEFT SUBTREAP\n";
     inorder(rad->left);
@@ -190,7 +191,7 @@ void afisareSplit(Nod* &rad,int key){
     inorder(rad->right);
     rad = stergere(rad,key);
 }
-Nod* copiere(Nod* &rad,Nod* &aux){
+Treap* copiere(Treap* &rad,Treap* &aux){
     if (rad == NULL)
         return NULL;
     copiere(rad->left,aux);
@@ -199,8 +200,8 @@ Nod* copiere(Nod* &rad,Nod* &aux){
 
     return aux;
 }
-void split(Nod* &rad,Nod* &st , Nod* &dr,int key){
-    Nod *subSt=NULL,*subDr=NULL;
+void split(Treap* &rad,Treap* &st , Treap* &dr,int key){
+    Treap *subSt=NULL,*subDr=NULL;
     rad = inserare(rad , key , INFINITY);
     subSt = copiere(rad->left,subSt);
     subDr = copiere(rad->right,subDr);
@@ -209,17 +210,17 @@ void split(Nod* &rad,Nod* &st , Nod* &dr,int key){
     dr = subDr;
 }
 
-Nod* join(Nod* &st,Nod* &dr){
+Treap* join(Treap* &st,Treap* &dr){
     if(maxim(st)->key < minim(dr)->key){
         int key = (maxim(st)->key + minim(dr)->key)/2;
-        Nod *aux = new Nod(key,INFINITY,st,dr);
+        Treap *aux = new Treap(key,INFINITY,st,dr);
         aux = stergere(aux,key);
         return aux;
     }
     return NULL;
 }
 
-Nod* reuniune(Nod* &st , Nod* &dr){
+Treap* reuniune(Treap* &st , Treap* &dr){
     if (dr != NULL){
         if(dr->left !=NULL)
             st = reuniune(st,dr->left);
@@ -228,4 +229,42 @@ Nod* reuniune(Nod* &st , Nod* &dr){
         st = inserare(st,dr->key,dr->prio);
     }
     return st;
+}
+
+int maximN(Treap* &rad, int key){
+    if(rad == NULL) return -1;
+    if(rad->key == key) return key;
+    else if(rad->key < key){
+        int k =maximN(rad->right , key);
+        if(k == -1)
+            return rad->key;
+        else
+            return k;
+        }
+    else if(rad->key > key){
+        return maximN(rad->left,key);
+    }
+}
+
+int minimN(Treap* &rad,int key){
+    if(rad == NULL) return -1;
+    if(rad->key == key) return key;
+    else if(rad->key > key){
+        int k = minimN(rad->left , key);
+        if( k == -1)
+            return rad-> key;
+        else
+            return k;
+        }
+    else if(rad->key < key){
+        return minimN(rad->right,key);
+    }
+}
+
+void interval(Treap* &rad,int key1 ,int key2){
+    if (rad == NULL)
+        return;
+    interval(rad->left,key1,key2);
+    if(rad->key >= key1 && rad->key <= key2) cout<<rad->key<<' ';
+    interval(rad->right,key1,key2);
 }
